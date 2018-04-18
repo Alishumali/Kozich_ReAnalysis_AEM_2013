7f4d23a3cb17806394a1f952d7825d4ce2697083
Alishum Ali <alishumali1@gmail.com>
2018-04-18 07:44:58 +0000

    Add code to obtain reference and raw data

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -1,38 +1,28 @@
 #!/user/bin/env bash
 
-## Analysis script
-
-# The following code is to download the manuscript's raw **fastq.gz**
-
-# *from the official mothur website - https://mothur.org/MiSeqDevelopmentData.html
-
-# * Compressed files will be unpacked and then .zip/.tar will be removed
-# The folowing script will be run from the project's root directory
-
-wget https://www.mothur.org/MiSeqDevelopmentData/StabilityWMetaG.tar
-tar xvf StabilityWMetaG.tar
-mv *.gz data/raw/
+# Download the raw data and put them into the data/raw directory
+wget --no-check-certificate https://www.mothur.org/MiSeqDevelopmentData/StabilityWMetaG.tar
+tar xvf StabilityWMetaG.tar -C data/raw/
 rm StabilityWMetaG.tar
 
+# Download the SILVA reference file (v.123). We will pull out the bacteria-specific sequences and
+# clean up the directories to remove the extra files
+wget http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
+tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
+code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
+mv silva.seed_v123.pick.align data/references/silva.seed.align
+rm Silva.seed_v123.tgz silva.seed_v123.*
+rm mothur.*.logfile
 
-# Obtained the RDP and Silva references alignment from mothur website:
-
-# downloading Silva, RDP files used in the manuscript
-## files as directed to be obtained from mothur website
-
-## RDP reference taxanomy
-wget https://www.mothur.org/w/images/8/88/Trainset14_032015.pds.tgz
+# Download the RDP taxonomy references (v14), put the necessary files in data/references, and
+# clean up the directories to remove the extra files
+wget -N http://www.mothur.org/w/images/8/88/Trainset14_032015.pds.tgz
 tar xvzf Trainset14_032015.pds.tgz
+mv trainset14_032015.pds/train* data/references/
+rm -rf trainset14_032015.pds
 rm Trainset14_032015.pds.tgz
-mv trainset14_032015.pds./* data/references/
 
-##Silva ref seq
-wget https://www.mothur.org/w/images/1/15/Silva.seed_v123.tgz
-tar xvzf Silva.seed_v123.tgz
-rm Silva.seed_v123.tgz
-mv *.gz data/references/
 
 # Generate a customized version of the SILVA reference database that targets the V4 region
 code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
 mv data/references/silva.seed.pcr.align data/references/silva.v4.align
-
